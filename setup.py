99508795555f545acd5a894a0803c097e13260ec
Levi McDonough <levimcdonough@gmail.com>
2016-04-19 17:15:47 -0400

    updates setup.py to exclude tests, adds classifiers.

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
 
-install_requires = ['flake8', 'setuptools']
-
 setup(
     name='cureatr-flake8-print',
     description="cureatr print statement and print functionn checker plugin for flake8",
@@ -10,11 +8,17 @@ setup(
     author_email='levi@cureatr.com',
     py_modules=['cureatr_flake8_print.cureatr_print_linter'],
     zip_safe=False,
-    packages=find_packages(),
-    install_requires=install_requires,
+    packages=find_packages(exclude=['tests*']),
+    install_requires=['flake8', 'setuptools'],
     entry_points={
         'flake8.extension': [
             'cureatr_flake8_print = cureatr_flake8_print.cureatr_print_linter:CureatrPrintLinter',
         ],
     },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+        ],
 )
