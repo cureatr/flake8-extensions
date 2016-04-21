779e52cce974a992f7e524509fe45b9a8a82acd1
Levi McDonough <levimcdonough@gmail.com>
2016-04-21 12:59:58 -0400

    updates tests to use enabled off by default

--- tests/test_cureatr_flake8_print.py
+++ tests/test_cureatr_flake8_print.py
@@ -13,7 +13,8 @@ class TestCureatrPrintLint(unittest.TestCase):
         self.print_function_data = ['from __future__ import print_function', '\n', 'print("hello world")']
         self.print_function_file = 'test_function_statement.py'
         self.print_ignore_file = 'reporting/tests_ignore.py'
-        self.ignore_argument = 'tests,tools,reporting'
+        self.ignore_argument = '^reporting/|^tools/|/tests/'
+        self.enable_extension = 'T002,T003'
         os.mkdir('reporting')
 
         with open(self.print_statement_file, 'w') as f:
@@ -27,23 +28,33 @@ class TestCureatrPrintLint(unittest.TestCase):
 
     def test_print_statement(self):
         cureatr_linter = cureatr_print_linter.CureatrPrintLinter(ast.parse('\n'.join(self.print_statement_data) + '\n', ), filename=self.print_statement_file)
-        cureatr_linter.ignore_printcheck_dirs = re.compile('|'.join(['^{}|/{}/'.format(option, option) for option in self.ignore_argument.split(',')])) if self.ignore_argument else None
+        cureatr_linter.ignore_path_regex = re.compile(self.ignore_argument) if self.ignore_argument else None
+        cureatr_linter.enable_extension = self.enable_extension
         self.assertIn('T002 print statement found.', next(cureatr_linter.run()))
+
+    def test_print_statement_not_enabled(self):
+        cureatr_linter = cureatr_print_linter.CureatrPrintLinter(ast.parse('\n'.join(self.print_statement_data) + '\n', ), filename=self.print_statement_file)
+        cureatr_linter.ignore_path_regex = re.compile(self.ignore_argument) if self.ignore_argument else None
+        cureatr_linter.enable_extension = ''
+        self.assertEqual(len(list(cureatr_linter.run())), 0)
 
     def test_print_function(self):
         cureatr_linter = cureatr_print_linter.CureatrPrintLinter(ast.parse('\n'.join(self.print_function_data) + '\n', ), filename=self.print_function_file)
-        cureatr_linter.ignore_printcheck_dirs = re.compile('|'.join(['^{}|/{}/'.format(option, option) for option in self.ignore_argument.split(',')])) if self.ignore_argument else None
+        cureatr_linter.ignore_path_regex = re.compile(self.ignore_argument) if self.ignore_argument else None
+        cureatr_linter.enable_extension = self.enable_extension
         self.assertIn('T003 print function found.', next(cureatr_linter.run()))
 
     def test_ignore_dirs(self):
         cureatr_linter = cureatr_print_linter.CureatrPrintLinter(ast.parse('\n'.join(self.print_function_data) + '\n', ), filename=self.print_ignore_file)
-        cureatr_linter.ignore_printcheck_dirs = re.compile('|'.join(['^{}|/{}/'.format(option, option) for option in self.ignore_argument.split(',')])) if self.ignore_argument else None
+        cureatr_linter.ignore_path_regex = re.compile(self.ignore_argument) if self.ignore_argument else None
+        cureatr_linter.enable_extension = self.enable_extension
         self.assertEqual(len(list(cureatr_linter.run())), 0)
 
     def test_no_arg(self):
         self.ignore_argument = ''
         cureatr_linter = cureatr_print_linter.CureatrPrintLinter(ast.parse('\n'.join(self.print_statement_data) + '\n', ), filename=self.print_ignore_file)
-        cureatr_linter.ignore_printcheck_dirs = re.compile('|'.join(['^{}|/{}/'.format(option, option) for option in self.ignore_argument.split(',')])) if self.ignore_argument else None
+        cureatr_linter.ignore_path_regex = re.compile(self.ignore_argument) if self.ignore_argument else None
+        cureatr_linter.enable_extension = self.enable_extension
         self.assertIn('T002 print statement found.', next(cureatr_linter.run()))
 
     def tearDown(self):
